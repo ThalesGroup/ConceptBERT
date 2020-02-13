c6056738280c6d1c7bf6ef4480d708cfd1c16c8a
Francois Garderes <francois.garderes@student.ecp.fr>
2020-02-13 18:33:54 -0500

    Changes type of input: list(list(ids)) instead of list(ids)

--- kilbert/graph_refinement/graph_refinement.py
+++ kilbert/graph_refinement/graph_refinement.py
@@ -22,7 +22,7 @@ class GraphRefinement(nn.Module):
         # Coefficient multiplied to the weight at each iteration
         self.attenuation_coef = 0.5
 
-    def compute_importance_index(self, sentence, list_word_attention):
+    def compute_importance_index(self, list_questions, list_question_attention):
         """
             Given a sentence, computes the importance index of each word
 
@@ -31,39 +31,39 @@ class GraphRefinement(nn.Module):
             Output: 
                 - `list_importance_indexes` (List[float]): list of the importance index of each word
         """
-        # TODO: Preprocess the question, so that the attention of any stop word or punctuation is 0
-
         # Given the word and its attention, computes the importance indexes of each one
-        list_words = sentence.split(" ")
+
         list_importance_indexes = []
-        for i, word in enumerate(list_words):
-            attention_word = list_word_attention[i]
-            list_importance_indexes.append(word, self.importance_index(attention_word))
+        for i, question in enumerate(list_questions):
+            question_importance_indexes = []
+            for j, word in enumerate(question):
+                attention_word = list_question_attention[i][j]
+                question_importance_indexes.append(
+                    self.importance_index(word, attention_word)
+                )
+            list_importance_indexes.append(question_importance_indexes)
 
-        return list_words, list_importance_indexes
+        return list_importance_indexes
 
-    def forward(self, question, attention_question, conceptnet_graph):
+    def forward(self, list_questions, attention_question, conceptnet_graph):
         """
             Refines `conceptnet_graph`, using the `question` and its `attention_question`
         """
-        list_words, list_importance_indexes = self.compute_importance_index(
-            question, attention_question
+        list_importance_indexes = self.compute_importance_index(
+            list_questions, attention_question
         )
-
-        # Convert the words to entities (in place to save memory)
-        for i, word in enumerate(len(list_words)):
-            list_words[i] = conceptnet_graph.get_index(word)
 
         # Update the weights in the graph
         # TODO: Try to find a way to compute it faster with less memory
-        for i, entity in enumerate(list_words):
-            # Initialize the edges
-            for edge in self.weight_edges:
-                conceptnet_graph.weight_edges[edge]["updated"] = False
-            # Propagate the weights for this entity
-            conceptnet_graph.propagate_weights(
-                [(entity, list_importance_indexes[i])],
-                self.propagation_threshold,
-                self.attenuation_coef,
-            )
+        for question in list_questions:
+            for i, entity in enumerate(question):
+                # Initialize the edges
+                for edge in self.weight_edges:
+                    conceptnet_graph.weight_edges[edge]["updated"] = False
+                # Propagate the weights for this entity
+                conceptnet_graph.propagate_weights(
+                    [(entity, list_importance_indexes[i])],
+                    self.propagation_threshold,
+                    self.attenuation_coef,
+                )
 
