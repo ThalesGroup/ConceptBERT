19750a0cb03e1d7df376baad80b03ac70b695af7
Francois Garderes <francois.garderes@student.ecp.fr>
2020-02-19 19:19:28 -0500

    Removes

--- kilbert/fusion_modules/cti_model/cti.py
+++ kilbert/fusion_modules/cti_model/cti.py
@@ -72,11 +72,6 @@ class CTIModel(nn.Module):
             b_emb[g] = self.t_net[g].forward_with_weights(
                 v_emb, q_emb, kg_emb, att[:, :, :, :, g]
             )
-
-            print("SHAPE WEIGHTS: ", att[:, :, :, :, g].shape)
-            temp_variable = b_emb[g].unsqueeze(1)
-            print("SHAPE TEMP: ", temp_variable.shape)
-
             q_emb = self.q_prj[g](b_emb[g].unsqueeze(1)) + q_emb
             kg_emb = self.kg_prj[g](b_emb[g].unsqueeze(1)) + kg_emb
 
