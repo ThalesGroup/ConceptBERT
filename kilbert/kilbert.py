a13f4c3fa4ac689842c36f9ef103f09292237b0b
Francois Garderes <francois.garderes@student.ecp.fr>
2020-02-25 18:19:54 -0500

    Converts tensor to double tensor

--- kilbert/kilbert.py
+++ kilbert/kilbert.py
@@ -208,7 +208,7 @@ class Kilbert(nn.Module):
                 # txt_embedding,
                 # pooled_output_t,
                 sequence_output_t,
-                kg_embedding,
+                kg_embedding.double(),
                 extended_attention_mask,
                 output_all_encoded_layers,
             )
